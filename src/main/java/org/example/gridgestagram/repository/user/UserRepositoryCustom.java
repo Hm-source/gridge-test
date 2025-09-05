f22a9fa1063162484a9f2946f3a80cd98efe6b21
Hm-source <swo8852@gmail.com>
2025-09-05 11:34:46 +0900

    feat: providerId 기반으로 구분

--- src/main/java/org/example/gridgestagram/repository/user/UserRepositoryCustom.java
+++ src/main/java/org/example/gridgestagram/repository/user/UserRepositoryCustom.java
@@ -2,6 +2,7 @@ package org.example.gridgestagram.repository.user;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.example.gridgestagram.repository.user.entity.QUser;
 import org.example.gridgestagram.repository.user.entity.User;
@@ -19,4 +20,15 @@ public class UserRepositoryCustom implements IUserRepositoryCustom {
             .where(QUser.user.name.trim().eq(name))
             .fetch();
     }
+
+    @Override
+    public Optional<User> findByProviderId(String providerId) {
+        return Optional.ofNullable(
+            jpaQueryFactory.selectFrom(QUser.user)
+                .where((QUser.user.providerId.eq(providerId)))
+                .fetchOne()
+        );
+    }
+
+
 }
