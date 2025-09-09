e3872d448ba047a21110c8e702fc98ea71b2c929
Hm-source <swo8852@gmail.com>
2025-09-10 00:03:31 +0900

    feat: 날짜 세팅 설정 추가 및 페이징 객체 설정 추가

---
+++ src/main/java/org/example/gridgestagram/config/WebConfig.java
@@ -0,0 +1,12 @@
+package org.example.gridgestagram.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
+public class WebConfig {
+
+}
