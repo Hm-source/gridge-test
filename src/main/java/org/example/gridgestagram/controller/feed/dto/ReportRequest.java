6dc77ebdc17d01a94da74c19abfc984390544878
Hm-source <swo8852@gmail.com>
2025-09-11 02:09:27 +0900

    feat: 신고 상세사유 삭제

--- src/main/java/org/example/gridgestagram/controller/feed/dto/ReportRequest.java
+++ src/main/java/org/example/gridgestagram/controller/feed/dto/ReportRequest.java
@@ -1,7 +1,6 @@
 package org.example.gridgestagram.controller.feed.dto;
 
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.example.gridgestagram.repository.feed.entity.vo.ReportReason;
@@ -16,7 +15,4 @@ public class ReportRequest {
 
     @NotNull(message = "신고 사유는 필수입니다")
     private ReportReason reason;
-
-    @Size(max = 200, message = "신고 상세 내용은 200자 이하로 입력해주세요")
-    private String description;
 }
