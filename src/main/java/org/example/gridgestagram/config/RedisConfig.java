7942897655b3eef7ed2b30ef35be253ff350ed26
Hm-source <swo8852@gmail.com>
2025-09-14 03:15:51 +0900

    fix: 기본값제거

--- src/main/java/org/example/gridgestagram/config/RedisConfig.java
+++ src/main/java/org/example/gridgestagram/config/RedisConfig.java
@@ -25,10 +25,10 @@ import org.springframework.util.StringUtils;
 @Slf4j
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
+    @Value("${spring.data.redis.host}")
     private String host;
 
-    @Value("${spring.redis.port}")
+    @Value("${spring.data.redis.port}")
     private int port;
 
     @Value("${spring.redis.timeout}")
