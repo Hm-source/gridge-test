537eda4283617590c2beed6001538f879194fa78
Hm-source <swo8852@gmail.com>
2025-09-19 01:25:02 +0900

    feat: 블랙리스트 관련 errorcode 등록

--- src/main/java/org/example/gridgestagram/exceptions/ErrorCode.java
+++ src/main/java/org/example/gridgestagram/exceptions/ErrorCode.java
@@ -96,8 +96,12 @@ public enum ErrorCode {
     LOG_SEARCH_FAILED("LOG_001", "로그 조회에 실패하였습니다.", 500),
     RECENT_LOGS_FAILED("LOG_002", "최신 로그를 조회하는데 실패하였습니다.", 500),
     USER_LOGS_FAILED("LOG_002", "사용자 로그를 조회하는데 실패하였습니다.", 500),
+
     // 공통
-    INVALID_REQUEST("COMMON_001", "유효하지 않은 요청입니다.", 400);
+    INVALID_REQUEST("COMMON_001", "유효하지 않은 요청입니다.", 400),
+    TOKEN_BLACKLISTED("COMMON_002", "차단된 토큰입니다.", 401),
+    USER_TOKENS_BLACKLISTED("COMMON_003", "사용자의 모든 토큰이 무효화되었습니다. 다시 로그인해주세요.", 401),
+    INVALID_REFRESH_TOKEN("COMMON_004", "유효하지 않은 리프레시 토큰입니다.", 401);
     private final String code;
     private final String message;
     private final int status;
